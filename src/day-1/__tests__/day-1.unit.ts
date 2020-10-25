0673264fd5ce320e69b28a38188dbf50eaad5a64
Lewis Barnes <lb.barnes@live.co.uk>
2020-10-25 16:54:58 +0000

    Completed Day 1: Part Two.

--- src/day-1/__tests__/day-1.unit.ts
+++ src/day-1/__tests__/day-1.unit.ts
@@ -1,17 +1,33 @@
-import { calculateFuel } from "../day-1";
+import { calculateFuel, calculateAdditionalFuel } from "../day-1";
 
-it("returns 2 for a mass of 12", () => {
-  expect(calculateFuel(12)).toEqual(2);
+describe("calculateFuel", () => {
+  it("returns 2 for a mass of 12", () => {
+    expect(calculateFuel(12)).toEqual(2);
+  });
+
+  it("returns 2 for a mass of 14", () => {
+    expect(calculateFuel(14)).toEqual(2);
+  });
+
+  it("returns 654 for a mass of 1969", () => {
+    expect(calculateFuel(1969)).toEqual(654);
+  });
+
+  it("returns 33583 for a mass of 100756", () => {
+    expect(calculateFuel(100756)).toEqual(33583);
+  });
 });
 
-it("returns 2 for a mass of 14", () => {
-  expect(calculateFuel(14)).toEqual(2);
-});
+describe("calculateAdditionalFuel", () => {
+  it("returns 2 for a mass of 14", () => {
+    expect(calculateAdditionalFuel(14)).toEqual(2);
+  });
 
-it("returns 654 for a mass of 1969", () => {
-  expect(calculateFuel(1969)).toEqual(654);
-});
+  it("returns 966 for a mass of 1969", () => {
+    expect(calculateAdditionalFuel(1969)).toEqual(966);
+  });
 
-it("returns 33583 for a mass of 100756", () => {
-  expect(calculateFuel(100756)).toEqual(33583);
+  it("returns 50346 for a mass of 100756", () => {
+    expect(calculateAdditionalFuel(100756)).toEqual(50346);
+  })
 });
--- src/day-1/day-1.ts
+++ src/day-1/day-1.ts
@@ -5,14 +5,31 @@ export const calculateFuel = (mass: number) => {
   return Math.floor(mass / 3) - 2;
 }
 
+export const calculateAdditionalFuel = (mass: number, total: number = 0) => {
+  const fuel = calculateFuel(mass);
+
+  if (fuel <= 0) {
+    return total;
+  }
+
+  return calculateAdditionalFuel(fuel, total + fuel);
+};
+
 export const calculatePartOne = () => {
   return readLines(path.join(__dirname, "input.txt"))
     .map((mass) => calculateFuel(parseInt(mass)))
     .reduce((result, x) => result + x, 0);
 };
 
+export const calculatePartTwo = () => {
+  return readLines(path.join(__dirname, "input.txt"))
+    .map((mass) => calculateAdditionalFuel(parseInt(mass)))
+    .reduce((result, x) => result + x, 0);
+};
+
 export const main = () => {
   console.log("Part One:", calculatePartOne());
+  console.log("Part Two:", calculatePartTwo());
 }
 
 if (require.main === module) {
