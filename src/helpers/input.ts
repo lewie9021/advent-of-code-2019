d0406fb62ddf9f6e5ead3c300ccc3c7ecb7e703e
Lewis Barnes <lb.barnes@live.co.uk>
2020-10-31 17:08:43 +0000

    Added several helpers for input files.

--- src/helpers/input.ts
+++ src/helpers/input.ts
@@ -1,7 +1,17 @@
 import * as fs from "fs";
 
+export const readTextFile = (path: string) => {
+  return fs.readFileSync(path, "utf-8");
+}
+
 export const readLines = (path: string) => {
-  const content = fs.readFileSync(path, "utf-8");
+  const content = readTextFile(path);
 
   return content.trim().split("\n");
+}
+
+export const readArray = (path: string) => {
+  const content = readTextFile(path);
+
+  return content.trim().split(",");
 }
